527101bad4665fab5e0c6cd412c6b1c91f8675d0
Salman Qureshi <isalmanqureshi@gmail.com>
2022-01-27 23:38:00 +0530

    Website update

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -29,7 +29,7 @@ return(
        <a href="https://github.com/hotheadhacker" target="_BLANK"><i className="fab fa-github fa-2x mx-2 github"></i></a>
        <a href="https://twitter.com/salmanually" target="_BLANK"><i className="fab fa-twitter fa-2x mx-2 twitter"></i></a>
        <a href="https://instagram.com/salmanually" target="_BLANK"><i className="fab fa-instagram fa-2x mx-2 instagram"></i></a>
-       <a href="https://isalman.xyz" target="_BLANK"><i className="fas fa-mouse-pointer fa-2x mx-2 website"></i></a>
+       <a href="https://isalman.dev" target="_BLANK"><i className="fas fa-mouse-pointer fa-2x mx-2 website"></i></a>
        <a href="mailto:isalmanqureshi@gmail.com" target="_BLANK"><i className="fas fa-envelope fa-2x mx-2 email"></i></a>
        <a href="https://blog.isalman.xyz" target="_BLANK"><i className="fas fa-blog fa-2x mx-2 blog"></i></a>
        </p>
