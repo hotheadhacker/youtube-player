cb2696d2ca403528004a7fb7549007da25643c16
Pedro Henrique <159962596+pedrohenriquebragap@users.noreply.github.com>
2025-02-25 01:59:33 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { LanguageProvider } from "./LanguageProvider";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <LanguageProvider>
     <App />
-  </React.StrictMode>
+  </LanguageProvider>
 );
